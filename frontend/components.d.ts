92b2018b1a29466b9a7a9d08ff30d6a50b8fa8d6
Cwj <3055269939@qq.com>
2026-06-13 15:02:31 +0800

    比特接入

--- frontend/components.d.ts
+++ frontend/components.d.ts
@@ -38,6 +38,7 @@ declare module 'vue' {
     ElMenu: typeof import('element-plus/es')['ElMenu']
     ElMenuItem: typeof import('element-plus/es')['ElMenuItem']
     ElOption: typeof import('element-plus/es')['ElOption']
+    ElPageHeader: typeof import('element-plus/es')['ElPageHeader']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRadio: typeof import('element-plus/es')['ElRadio']
     ElRadioGroup: typeof import('element-plus/es')['ElRadioGroup']
